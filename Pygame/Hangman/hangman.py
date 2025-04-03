904aef05844b8fb901813a262a4b6fedcedad89e
Tuan Ton <tonthattuanst@gmail.com>
2025-04-02 21:23:39 -0700

    reset game function

--- Pygame/Hangman/hangman.py
+++ Pygame/Hangman/hangman.py
@@ -16,7 +16,7 @@ BLACK = (0, 0, 0)
 GREY = (128, 128, 128)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-PASTEL_BLUE = (186,225, 255)
+PASTEL_BLUE = (186, 225, 255)
 
 # Clock for controlling the frame rate
 clock = pygame.time.Clock()
@@ -34,6 +34,9 @@ correct = 0
 flag = 0
 chances = 5
 PI = 3.14
+# Main game loop
+running = True
+
 
 label_x = 200
 
@@ -77,6 +80,7 @@ heart_image = pygame.transform.scale(heart_image, (50, 50))
 
 # function to draw a circle with triangles
 
+
 def draw_lines_in_circle(surface, center_x, center_y, radius, num_lines, color):
     angle_step = 360 / num_lines
     for i in range(num_lines):
@@ -84,12 +88,13 @@ def draw_lines_in_circle(surface, center_x, center_y, radius, num_lines, color):
         rad = math.radians(angle)
         x = center_x + radius * math.cos(rad)
         y = center_y - radius * math.sin(rad)
-        
+
         # Draw the line from the center to the edge of the circle
         pygame.draw.line(surface, color, (center_x, center_y), (x, y), 2)
-        
+
+
 def draw_top_right_quadrant(surface, center_x, center_y, radius, start_angle, end_angle, color):
-    
+
     start_angle = int(start_angle)
     end_angle = int(end_angle)
     # Define the points for the filled top-right quadrant
@@ -111,23 +116,27 @@ def draw_top_right_quadrant(surface, center_x, center_y, radius, start_angle, en
     # Draw the filled polygon
     pygame.draw.polygon(surface, color, points)
 
+
 def step_angle(total_deg_in_circle, num_lines):
     # Generate angles from start_angle to end_angle with a step
     angle = 360 / num_lines
     return angle
 
 # draw text prize in each section of the wheel
+
+
 def draw_text_prize(surface, center_x, center_y, radius, num_lines, color, text):
     # Adjust the angle to keep the text readable
-    track=0
-    step = step_angle(360,num_lines)
-    for i in range(int(step/2),360,int(step)):  # Loop through angles in 45-degree increments
+    track = 0
+    step = step_angle(360, num_lines)
+    # Loop through angles in 45-degree increments
+    for i in range(int(step/2), 360, int(step)):
         angle = i
         # Convert degrees to radians
         radians = angle * (PI / 180)
-        
+
         text_radius = radius * 0.70
-        
+
         x = center_x + text_radius * math.cos(radians)
         y = center_y + text_radius * math.sin(radians)
         text_surface = font_small_bold.render(text[track], True, color)
@@ -141,10 +150,12 @@ def draw_text_prize(surface, center_x, center_y, radius, num_lines, color, text)
 
         # Rotate the text to match the angle of the line
         rotated_text = pygame.transform.rotate(text_surface, -adjusted_angle)
-        text_rect = rotated_text.get_rect(center=(x, y))  # Center the text at the calculated position
+        # Center the text at the calculated position
+        text_rect = rotated_text.get_rect(center=(x, y))
         # Draw the text on the surface
         surface.blit(rotated_text, text_rect)
-        
+
+
 # Function to draw word boxes
 def draw_word_boxes(screen, word, clicked_buttons, font, box_color, text_color, box_width, box_height, start_x, start_y):
     for i, letter in enumerate(word):
@@ -160,8 +171,7 @@ def draw_word_boxes(screen, word, clicked_buttons, font, box_color, text_color, 
             text_rect = text_surface.get_rect(center=rect.center)
             screen.blit(text_surface, text_rect)
 
-# Main game loop
-running = True
+
 def show_remaining_lives(WIDTH, screen, chances, heart_image):
     heart_x = WIDTH/2 - (chances * heart_image.get_width()) / 2
     heart_y = 50
@@ -169,21 +179,22 @@ def show_remaining_lives(WIDTH, screen, chances, heart_image):
         screen.blit(heart_image, (heart_x + i *
                     heart_image.get_width(), heart_y))
 
+
 def draw_alphabet_buttons(screen, WHITE, BLACK, GREY, clicked_buttons, alphabet, alphaLetterBox, alphaButtonGap, alphabet_button, font_small, start_x_button, start_y_button):
     for i in range(26):
         x = start_x_button + (i % 7) * (alphaLetterBox + alphaButtonGap)
         y = start_y_button + (i // 7) * (alphaLetterBox + alphaButtonGap)
         rect = pygame.Rect(x, y, alphaLetterBox, alphaLetterBox)
         alphabet_button[alphabet[i]] = rect
-        
+
         # check if the button has been clicked
         if alphabet[i] in clicked_buttons:
             transparent_surface = pygame.Surface(
-                            (alphaLetterBox, alphaLetterBox), pygame.SRCALPHA)
+                (alphaLetterBox, alphaLetterBox), pygame.SRCALPHA)
 
             # Draw the button with transparency
             pygame.draw.rect(transparent_surface, GREY, (0, 0,
-                            alphaLetterBox, alphaLetterBox), border_radius=10)
+                                                         alphaLetterBox, alphaLetterBox), border_radius=10)
             # Draw the letter inside the button
             letter = font_small.render(alphabet[i], True, WHITE)
             text_width, text_height = letter.get_size()
@@ -194,10 +205,10 @@ def draw_alphabet_buttons(screen, WHITE, BLACK, GREY, clicked_buttons, alphabet,
             transparent_surface.blit(letter, (x, y))
             # Draw the transparent surface on the screen
             screen.blit(transparent_surface, (rect.x, rect.y))
-        else: 
+        else:
             pygame.draw.rect(screen, BLACK, rect, border_radius=10)
             pygame.draw.rect(screen, WHITE, rect, 2, border_radius=10)
-        
+
         # Draw the letter inside the button
         letter = font_small.render(alphabet[i], True, WHITE)
         text_width, text_height = letter.get_size()
@@ -206,6 +217,21 @@ def draw_alphabet_buttons(screen, WHITE, BLACK, GREY, clicked_buttons, alphabet,
         y = rect.y + (alphaLetterBox - text_height) / 2
         # Draw the letter
         screen.blit(letter, (x, y))
+
+
+def draw_game_over_message(screen, message, font, color):
+    text_surface = font.render(message, True, color)
+    text_rect = text_surface.get_rect(center=(WIDTH/2, HEIGHT/2))
+    screen.blit(text_surface, text_rect)
+
+
+# Function to reset the game
+def reset_game():
+    global word, clicked_buttons, chances
+    word = random.choice(words)
+    clicked_buttons.clear()
+    chances = 5
+
 
 while running:
     for event in pygame.event.get():
@@ -230,9 +256,9 @@ while running:
                     if rect.collidepoint(mouse_x, mouse_y):
                         if letter in clicked_buttons:
                             continue
-                        
+
                         clicked_buttons.append(letter)
-                        
+
                         if letter in word:
                             clicked_buttons.append(letter)
                         else:
@@ -240,9 +266,30 @@ while running:
                             chances -= 1
                             print(f"Chances left: {chances}")
                             if chances == 0:
-                                print("You lost")
-                                running = False
+                                # display game over message on screen
+                                draw_game_over_message(
+                                    screen, "Game Over!", font, RED)
+                                pygame.display.flip()
+                                pygame.time.delay(2000)  # Pause for 2 seconds
+                                reset_game()
+
                         break
+
+# Spinning the wheel
+    if (spinning):
+        draw_top_right_quadrant(
+            screen, wheelX, wheelY, wheelRadius, rotation_angle, rotation_angle + 45, GREEN)
+        rotation_angle = (rotation_angle + rotation_speed) % 360
+        # Decrease the speed gradually
+        rotation_speed = max(rotation_speed-0.1, 0)
+        spin_duration -= clock.get_time() / 1000
+        if spin_duration <= 0:
+            spinning = False
+            # determine the target section
+            target_section = round(
+                (rotation_angle % 360) / 45) % 8
+            rotation_angle = target_section * 45
+            print("Spin finished")
 
     # Game logic here
 
@@ -259,14 +306,15 @@ while running:
     start_x = (WIDTH - total_word_width) / 2.0
     start_y = 450
 
-    draw_word_boxes(screen, word, clicked_buttons, font, WHITE, BLACK, wordBoxWidth, wordBoxLength, start_x, start_y)
+    draw_word_boxes(screen, word, clicked_buttons, font, WHITE,
+                    BLACK, wordBoxWidth, wordBoxLength, start_x, start_y)
 
-
-    start_x_button = (WIDTH / 2 + (WIDTH / 2 - total_box_width) / 2) + alphaLetterBox
-    start_y_button = 100
     # Draw alphabet buttons
-    draw_alphabet_buttons(screen, WHITE, BLACK, GREY, clicked_buttons, alphabet, alphaLetterBox, alphaButtonGap, alphabet_button, font_small, start_x_button, start_y_button)
-        
+    start_x_button = (
+        WIDTH / 2 + (WIDTH / 2 - total_box_width) / 2) + alphaLetterBox
+    start_y_button = 100
+    draw_alphabet_buttons(screen, WHITE, BLACK, GREY, clicked_buttons, alphabet, alphaLetterBox,
+                          alphaButtonGap, alphabet_button, font_small, start_x_button, start_y_button)
 
     # Draw wheel
     pygame.draw.circle(screen, BLACK, (wheelX, wheelY), wheelRadius)
@@ -286,25 +334,10 @@ while running:
     draw_top_right_quadrant(screen, wheelX, wheelY, wheelRadius,
                             rotation_angle, rotation_angle + 45, GREEN)
 
-    if (spinning):
-        draw_top_right_quadrant(
-            screen, wheelX, wheelY, wheelRadius, rotation_angle, rotation_angle + 45, GREEN)
-        rotation_angle = (rotation_angle + rotation_speed) % 360
-        # Decrease the speed gradually
-        rotation_speed = max(rotation_speed-0.1, 0)
-        spin_duration -= clock.get_time() / 1000
-        if spin_duration <= 0:
-            spinning = False
-            # determine the target section
-            target_section = round(
-                (rotation_angle % 360) / 45) % 8
-            rotation_angle = target_section * 45
-            print("Spin finished")
-
     # Draw the heart representing the chances left
+
     show_remaining_lives(WIDTH, screen, chances, heart_image)
 
-        
     # Draw the lines to divide sections in the circle
     draw_lines_in_circle(screen, wheelX, wheelY, wheelRadius, 8, WHITE)
     draw_text_prize(screen, wheelX, wheelY, wheelRadius,
